971900090099f34ac999e07f807cdbd353664fa8
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-21 14:18:47 +0530

    create todo

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -12,4 +12,19 @@ router.get("/", function(req, res) {
   });
 });
 
+// router.get("/new", function(req, res) {
+//   res.render("todos/new");
+// });
+
+router.post("/", function(req, res) {
+  Todo.create(req.body.todo, function(err, todo) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("TODO Created");
+      res.redirect("/todos");
+    }
+  });
+});
+
 module.exports = router;
