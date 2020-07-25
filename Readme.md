4c617d3de1ae4f9ba9a4a1ba230eba1b93cdbe43
Cruise <cruise@Nishants-MacBook-Air.local>
2020-07-25 11:38:58 +0530

    Todo Restful Routes

--- Readme.md
+++ Readme.md
@@ -4,3 +4,10 @@
 
 - TODO list
   - RESTful TODO
+    - List Routes: Done
+    - New Route: Not required
+    - Create Route: Done
+    - Show Route: Not required
+    - Edit Route: Done
+    - Update Route: Done
+    - Delete Route: Done
--- app/routes/todos.js
+++ app/routes/todos.js
@@ -7,14 +7,10 @@ router.get("/", function(req, res) {
     if (err) {
       res.send("Error"); 
     } else {
-      res.render("todos/index", {todos: todos});
+      res.render("todos/index", {todos: todos, id: null});
     }
   });
 });
-
-// router.get("/new", function(req, res) {
-//   res.render("todos/new");
-// });
 
 router.post("/", function(req, res) {
   Todo.create(req.body.todo, function(err, todo) {
@@ -22,6 +18,38 @@ router.post("/", function(req, res) {
       res.send(err);
     } else {
       console.log("TODO Created");
+      res.redirect("/todos");
+    }
+  });
+});
+
+router.get("/:id/edit", function(req, res) {
+  Todo.find({}, function(err, todos) {
+    if (err) {
+      res.send("Error"); 
+    } else {
+      res.render("todos/index", {todos: todos, id: req.params.id});
+    }
+  });
+});
+
+router.put("/:id", function(req, res) {
+  Todo.findByIdAndUpdate(req.params.id, req.body.todo, function(err, todo) {
+    if(err) {
+      res.send(err);
+    } else {
+      console.log("TODO Updated");
+      res.redirect("/todos");
+    }
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  Todo.findByIdAndDelete(req.params.id, function(err) {
+    if(err) { 
+      res.send(err);
+    } else {
+      console.log("TODO Deleted");
       res.redirect("/todos");
     }
   });
